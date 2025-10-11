100dddf7ab30cc1f32e076ebd694ff9903523ca5
Mohammad Irshad <awairit2k20@gmail.com>
2025-10-11 16:00:52 +0530

    ClassLevelDataMember Work By Notes

--- Data Member/class_ex_1.py
+++ Data Member/class_ex_1.py
@@ -2,3 +2,10 @@ class ClassDataMember:
     subject= "Python"
 
 print("The class level data member 'student' value is {} of class 'ClassDataMember'".format(ClassDataMember.subject))
+
+#ClassLevelDataMembersEx1.py
+class Student:
+    crs="PYTHON" # Here crs is called Class Level Data Member
+
+#main Program
+print("Course name=",Student.crs)
