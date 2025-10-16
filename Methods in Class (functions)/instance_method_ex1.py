e74d03f0a2eba058e2055aedcf8110f97f3f08a4
Mohammad Irshad <awairit2k20@gmail.com>
2025-10-16 05:31:00 +0530

    Using of 'self' in instance method to address instance DataMembar and magic variable called '__dict__' for getting all the insrance datamember for an onhect in Dictionary

--- Methods in Class (functions)/instance_method_ex1.py
+++ Methods in Class (functions)/instance_method_ex1.py
@@ -58,3 +58,39 @@ print("---------------------------------------------------")
 s1.dispstuddet("First")
 #call instance method dispstudvals() for displying the values of s2
 s2.dispstuddet("Second")
+
+#================================================================
+
+#Program for Reading the Values of Student by using Instance Method in OOPs
+#InstanceMethodEx3.py
+class Student3:
+    def readstudvals(self,objinfo):
+        print("Enter {} Object Information".format(objinfo))
+        self.sno=int(input("\tEnter Student Number:"))
+        self.name =input("\tEnter Student Name:")
+        self.marks=float(input("\tEnter Student Marks:"))
+    def dispstuddet(self,objinfo):
+        print("-"*50)
+        print("{} Student Information".format(objinfo))
+        print("-" * 50)
+        print("\tStudent Number:{}".format(self.sno))
+        print("\tStudent Name:{}".format(self.name))
+        print("\tStudent Marks:{}".format(self.marks))
+        print("-" * 50)
+#Main Program
+print("---------------------------------------------------")
+s1=Student3()
+s2=Student3()
+print("Content of s1 before reading=",s1.__dict__)
+print("Content of s2 before reading=",s2.__dict__)
+print("---------------------------------------------------")
+#Call Instance Method readstudvals() for rading the Values of s1
+s1.readstudvals("First")
+#call instance method dispstudvals() for displying the values of s1
+s1.dispstuddet("First")
+print("---------------------------------------------------")
+#Call Instance Method readstudvals() for rading the Values of s2
+s2.readstudvals("Second")
+print("---------------------------------------------------")
+#call instance method dispstudvals() for displying the values of s2
+s2.dispstuddet("Second")
