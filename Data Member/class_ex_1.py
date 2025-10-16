299e2b8bd758155fc8f4e02fac4e2af949fe4e56
Mohammad Irshad <awairit2k20@gmail.com>
2025-10-16 08:34:39 +0530

    Minor twiks for better user Experiance

--- Data Member/class_ex_1.py
+++ Data Member/class_ex_1.py
@@ -3,12 +3,17 @@ class ClassDataMember:
 
 print("The class level data member 'student' value is {} of class 'ClassDataMember'".format(ClassDataMember.subject))
 
+#=======================================================================================================================
+
+
 #ClassLevelDataMembersEx1.py
 class Student:
     crs="PYTHON" # Here crs is called Class Level Data Member
 
 #main Program
 print("Course name=",Student.crs)
+
+#=======================================================================================================================
 
 #ClassLevelDataMembersEx2.py
 class Student:pass
