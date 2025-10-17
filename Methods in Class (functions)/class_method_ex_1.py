77d44138157ff4c80870d5c3b21524949a1afdcc
Mohammad Irshad <awairit2k20@gmail.com>
2025-10-17 06:37:34 +0530

    Calling a method and data member by both ways in class level data member by using Class.vaiableName and cls.variableName (here 'cls' is keyword) while on other side calling a Class level Method by creating Object of Class as Obj.ClassMethodName(or Obj.func) and ClassName.ClassMethodName(or Obj.func)

--- Methods in Class (functions)/class_method_ex_1.py
+++ Methods in Class (functions)/class_method_ex_1.py
@@ -1,43 +1,80 @@
-class ClassDataMethod:
-    @classmethod
-    def class_method(cls):
-        cls.subject="Python"
-        cls.intrest="Programming"
-method=ClassDataMethod()
-method.class_method()
-print("-"*50)
-# print("I am learning {} and my intrest in {}".format(cls.subject,cls.intrest)) #NameError: name 'cls' is not defined
-
-print("\nI am learning {} and my intrest is in {}".format(ClassDataMethod.subject,ClassDataMethod.intrest))
+# class ClassDataMethod:
+#     @classmethod
+#     def class_method(cls):
+#         cls.subject="Python"
+#         cls.intrest="Programming"
+# method=ClassDataMethod()
+# method.class_method()
+# print("-"*50)
+# # print("I am learning {} and my intrest in {}".format(cls.subject,cls.intrest)) #NameError: name 'cls' is not defined
+#
+# print("\nI am learning {} and my intrest is in {}".format(ClassDataMethod.subject,ClassDataMethod.intrest))
+#
+# #=======================================================================================================================
+#
+# class ClassDataMethodEx2:
+#     @classmethod
+#     def class_method(cls):
+#         ClassDataMethodEx2.subject="Django"
+#         ClassDataMethodEx2.intrest="Framework"
+# method=ClassDataMethodEx2()
+# method.class_method()
+# print("-"*50)
+# print("\nI am learning {} and my intrest is in {}".format(ClassDataMethodEx2.subject,ClassDataMethodEx2.intrest))
+#
+# #=======================================================================================================================
+#
+# class ClassDataMethodEx3:
+#     # 1. Define the class attributes EXPLICITLY to satisfy the IDE
+#     subject = None  # Explicitly defined class attribute
+#     intrest = None  # Explicitly defined class attribute
+#
+#     @classmethod
+#     def class_method(cls):
+#         # 2. Assign new values to the existing class attributes
+#         # We can use 'cls' instead of the full class name for cleaner code
+#         cls.subject = "Django"
+#         cls.intrest = "Framework"
+#
+# method = ClassDataMethodEx3()
+# method.class_method() # This updates the class attributes
+#
+# print("-" * 50)
+# print("\nI am learning {} and my intrest is in {}".format(ClassDataMethodEx3.subject, ClassDataMethodEx3.intrest))
 
 #=======================================================================================================================
 
-class ClassDataMethodEx2:
+class ClassDataMethodEx4:
     @classmethod
-    def class_method(cls):
-        ClassDataMethodEx2.subject="Django"
-        ClassDataMethodEx2.intrest="Framework"
-method=ClassDataMethodEx2()
-method.class_method()
-print("-"*50)
-print("\nI am learning {} and my intrest is in {}".format(ClassDataMethodEx2.subject,ClassDataMethodEx2.intrest))
 
-#=======================================================================================================================
+    def subject(cls, perimeter):
+        cls.subject2="Python"
+        try:
+            ClassDataMethodEx4.rollnumber= int(input("Enter the Roll Number: ")) #ClassDataMethodEx4.rollnumber = cls.rollnumber
+        except Exception as e:
+            print(e)
+        else:
+            print(input("Enter the Student Name: "))
+            print("Enter the Roll Number: {}".format(cls.rollnumber))
+            print("Roll Number =",ClassDataMethodEx4.rollnumber)
 
-class ClassDataMethodEx3:
-    # 1. Define the class attributes EXPLICITLY to satisfy the IDE
-    subject = None  # Explicitly defined class attribute
-    intrest = None  # Explicitly defined class attribute
+        finally:
+            print("The Subject is: {}".format(cls.subject2))
+            print("The FrameWork is: {}" .format(perimeter))
+    #     cls.framework()
+    # @classmethod
+    # def framework(self ):
+    #     print("The Framework is: {} and swift".format(ClassDataMethodEx4.framework))
 
-    @classmethod
-    def class_method(cls):
-        # 2. Assign new values to the existing class attributes
-        # We can use 'cls' instead of the full class name for cleaner code
-        cls.subject = "Django"
-        cls.intrest = "Framework"
 
-method = ClassDataMethodEx3()
-method.class_method() # This updates the class attributes
+obj_call=ClassDataMethodEx4()
+obj_call.subject("Django")
 
-print("-" * 50)
-print("\nI am learning {} and my intrest is in {}".format(ClassDataMethodEx3.subject, ClassDataMethodEx3.intrest))
+print("="*30)
+
+print(obj_call.subject2)
+
+print("="*30)
+
+ClassDataMethodEx4.subject("Swift")
+
